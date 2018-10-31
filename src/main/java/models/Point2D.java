b1c127c5706ada66481b2dff30b68186718df3ed
Maximilian Kamp <Maximilian.Kamp@mni.thm.de>
2018-10-31 15:15:10 +0100

    2D Punkte

---
+++ src/main/java/models/Point2D.java
@@ -0,0 +1,51 @@
+package models;
+
+
+public class Point2D {
+    private int x;
+    private int y;
+    private String name;
+
+    public Point2D(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double calcDistance(Point2D p) {
+        double x = Math.pow(this.x - p.getX(), 2);
+        double y = Math.pow(this.y - p.getY(), 2);
+        return Math.sqrt(x + y);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Point2D{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
